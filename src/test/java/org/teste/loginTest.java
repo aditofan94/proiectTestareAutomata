d81bd5cdf14d8a97d6f581eaa31c91d49835019b
Adi Tofan <adi_tofan@yahoo.com>
2021-01-12 18:46:10 +0200

    update 4

--- src/test/java/org/teste/loginTest.java
+++ src/test/java/org/teste/loginTest.java
@@ -6,16 +6,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.security.Key;
+
 public class loginTest {
 
     @Test
-    public void loginWithTestEmail () {
+    public void loginAttempt () {
         System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.emag.ro/");
+        driver.get("https://www.linkedin.com/");
 
-        driver.findElement(By.id("searchboxTrigger")).sendKeys("mouses" + Keys.ENTER);
-
+        driver.findElement(By.id("session_key")).sendKeys("adi_tofan@yahoo.com" + Keys.ENTER);
+        driver.findElement(By.id("session_password")).sendKeys("testpassword" + Keys.ENTER);
     }
 }
