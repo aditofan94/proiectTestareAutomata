0b781a003b6fd65264e9d7f752ad745995e9e390
Adi Tofan <adi_tofan@yahoo.com>
2021-01-13 21:38:06 +0200

    update 6

--- src/test/java/org/teste/testareAutomata.java
+++ src/test/java/org/teste/testareAutomata.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
+
 
 public class testareAutomata {
 
@@ -16,19 +16,20 @@ public class testareAutomata {
         System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        //search test with one word
         driver.get("https://www.emag.ro/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
         driver.findElement(By.id("searchboxTrigger")).sendKeys("mouse" + Keys.ENTER);
 
+        //log in attempt
         driver.get("https://www.linkedin.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
         driver.findElement(By.id("session_key")).sendKeys("123testemail" + Keys.ENTER);
         driver.findElement(By.id("session_password")).sendKeys("123testpassword" + Keys.ENTER);
-
+        
         driver.close();
+
     }
 }
